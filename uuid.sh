a295b883fe1dd7aed665ab589b55d847b5151c5b
Tony <root@pc>
2021-02-20 22:11:49 -0500

    add another UUID format

--- uuid.sh
+++ uuid.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # autogen UUID
-uuid=$(cat /proc/sys/kernel/random/uuid)
-echo "Random UUID: "$uuid
+uuid1=$(cat /proc/sys/kernel/random/uuid)
+uuid2=$(cat /proc/sys/kernel/random/uuid | sed 's/-//g')
+
+echo "Random UUID-1: "$uuid1
+echo "Random UUID-2: "$uuid2
+
+
