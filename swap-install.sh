449448abf5a2b071f407678dc67ef7888bfb40ca
Tony <root@pc>
2021-04-21 22:24:46 -0400

    fix error

--- swap-install.sh
+++ swap-install.sh
@@ -3,7 +3,7 @@
 cat << EOF
 #
 # swap.sh
-# This shell scipts will create 512M SWAP file.
+# This shell scipts will create SWAP file.
 #
 EOF
 
@@ -12,6 +12,13 @@ free -h
 echo ""
 swapon
 echo ""
+
+    if [ -f "/var/swapfile" ]; then
+        echo "/var/swapfile already exist!"
+        echo "exit..."
+        exit 1
+    fi
+
 echo "Before create SWAP file, make sure the SWAP file doesn't exist."
 
 read -p "Please input \"YES\" to continue: " answer
@@ -21,31 +28,23 @@ case $answer in
     echo "continue..."
 
 
-## make choice
+## choose swap size
 read -p "Please input SWAP size (GB): [0.5/1/2/3/4] " swapsize
-case $swapsize in  
-
 
 ## create SWAP
-    if command -v fallocate >/dev/null 2>&1; then
-        fallocate -l $swapsizeG /var/swapfile
-    else
-        dd if=/dev/zero of=/var/swapfile bs=1G count=$swapsize
-    fi
+fallocate -l $swapsize\G /var/swapfile
 
+chmod 0600 /var/swapfile
 mkswap /var/swapfile
 swapon /var/swapfile
 echo "/var/swapfile swap swap defaults 0 0" >>/etc/fstab
-
-chmod 0600 /var/swapfile
 
 free -m
 echo ""
 swapon --show
 ls -lh /var/swapfile
 echo ""
-echo "$swapsizeG SWAP file has been created!"
-
+echo $swapsize"G SWAP file has been created!"
 
 
 ## go exit
