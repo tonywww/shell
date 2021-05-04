4d19befe56ca6d6458bb2f736913975bfe20a2d6
Tony <root@pc>
2021-05-03 20:48:28 -0400

    add CentOS support,code formatted

--- docker-install.sh
+++ docker-install.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
 
-echo ""
-echo "systemctl status docker"
-systemctl status docker
-echo ""
-read -p "Are you sure to install Docker CE? [y/n} " answer
+cat <<EOF
+#
+# docker-install.sh
+# Support OS: Debian / Ubuntu / CentOS
+#
+# This shell scipts will install Docker CE.
+#
+EOF
+
+no_command() {
+    if ! command -v $1 >/dev/null 2>&1; then
+        if [ -z "$3" ]; then
+            $2 install -y $1
+        else
+            $2 install -y $3
+        fi
+    fi
+}
+
+read -p "Please press \"y\" to continue: " answer
 
 case $answer in
-    Y|y)
+Y | y)
     echo "continue..."
 
-apt update -y
-apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update -y
-apt install -y docker-ce
+    #check OS
+    source /etc/os-release
 
-systemctl status docker --no-pager
-echo ""
+    case $ID in
+    debian | ubuntu)
+        echo System OS is $PRETTY_NAME
+        apt update
+        no_command curl apt
 
+        apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+        curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+        add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+        apt update
+        apt install -y docker-ce
+        ;;
 
-## go exit
+    centos | fedora | rhel | sangoma)
+        echo System OS is $PRETTY_NAME
+
+        yum install -y yum-utils
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum install -y docker-ce
+        ;;
+    esac
+
+    systemctl status docker --no-pager
+    echo ""
+
+    ## go exit
     ;;
 
-    *)
+*)
     echo "exit"
     ;;
 esac
