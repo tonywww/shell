6d26bf42f13a861f988d570a4333a763e23ed7b4
Tony <root@pc>
2021-05-03 20:52:57 -0400

    add install lsb_release

--- docker-install.sh
+++ docker-install.sh
@@ -33,6 +33,7 @@ Y | y)
         echo System OS is $PRETTY_NAME
         apt update
         no_command curl apt
+        no_command lsb_release apt lsb-release
 
         apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
         curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
